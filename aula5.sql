3ed7162dc1f2481247d8516d2d819ddefb1b308b
MARIANA EGYPTO FEIGEL <mariana.0769@aluno.pr.senac.br>
2022-08-24 22:24:49 -0300

    aula 5 inner join

--- aula5.sql
+++ aula5.sql
@@ -1,9 +1,11 @@
-CREATE DATABASE IF NOT EXISTS aprenderjoin CHARACTER SET utf8mb4;
+CREATE DATABASE IF NOT EXISTS aprenderjoin CHARACTER SET utf8mb4; --cria a base de dados se ela nao existe
 
 USE aprenderjoin;
 
-DROP TABLE IF EXISTS cidade;
-DROP TABLE IF EXISTS estado;
+DROP TABLE IF EXISTS restaurantes; 
+DROP TABLE IF EXISTS cidade;--se já existirem deleta
+DROP TABLE IF EXISTS estado;--se já existirem deleta
+
 
 CREATE TABLE IF NOT EXISTS estado(
     id_estado INTEGER PRIMARY KEY AUTO_INCREMENT,
@@ -28,9 +30,9 @@ VALUES
     ('Bahia', 'nordeste'),
     ('Pernambuco', 'nordeste'),
     ('Para', 'norte'),
-    ('Mato Grosso', 'centro-oeste');
+    ('Mato Grosso', 'centro-oeste');        
 
-SET @idParana:=(SELECT id_estado FROM estado WHERE estado.nome = 'Parana');
+SET @idParana:=(SELECT id_estado FROM estado WHERE estado.nome = 'Parana');--set é criar uma variavel
 SET @idSaoPaulo:=(SELECT id_estado FROM estado WHERE estado.nome = 'Sao Paulo');
 SET @idRioDeJaneiro:=(SELECT id_estado FROM estado WHERE estado.nome = 'Rio de Janeiro, PAH PAH TAH TAH');
 SET @idBahia:=(SELECT id_estado FROM estado WHERE estado.nome = 'Bahia');
@@ -38,9 +40,141 @@ SET @idPernambuco:=(SELECT id_estado FROM estado WHERE estado.nome = 'Pernambuco
 SET @idPara:=(SELECT id_estado FROM estado WHERE estado.nome = 'Para');
 SET @idMatoGrosso:=(SELECT id_estado FROM estado WHERE estado.nome = 'Mato Grosso');
 
+INSERT INTO cidade(nome, id_estado)VALUES
+    ('Curitiba', @idParana),
+    ('Londrina', @idParana),
+    ('Paranagua', @idParana),
+    ('Sao Paulo', @idSaoPaulo),
+    ('Sorocaba', @idSaoPaulo),
+    ('Rio de Janeiro', @idRioDeJaneiro),
+    ('Niteroi', @idRioDeJaneiro),
+    ('Mage', @idRioDeJaneiro),
+    ('Porto Real', @idRioDeJaneiro),
+    ('Petropolis', @idRioDeJaneiro),
+    ('Salvador', @idBahia),
+    ('Feira de Santana', @idBahia),
+    ('Recife', @idPernambuco),
+    ('Olinda', @idPernambuco),
+    ('Petrolina', @idPernambuco),
+    ('Belem', @idPara),
+    ('Cuiaba', @idMatoGrosso);
 
-/*CREATE TABLE cidade(
-    id_cidade INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255) NOT NULL,
-    estado VARCHAR(255) NOT NULL
-);*/
+/* 
+SELECT
+    estado.nome
+FROM
+    estado
+WHERE
+    estado.id_estado=(
+        SELECT 
+            cidade.id_estado
+        FROM
+            cidade
+        WHERE
+            cidade.nome = 'Petropolis'
+    );
+SELECT 
+    *
+FROM
+    cidade
+INNER JOIN /*quando vc usa o join entre duas tabelas o banco faz uma operação, pega as duas tabelas e junta elas, vira uma tabela só, esse operação melhora a performance da pesquisa. Pois usando subselect executa duas operações, pro primeiro e pro segundo select embutido.Trás tudo das duas tabelas
+    estado ON cidade.id_estado = estado.id_estado
+WHERE
+    nome='Petropolis';*/
+
+
+CREATE TABLE restaurantes (
+    id_restaurante INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    descricao VARCHAR(255),
+    id_cidade INTEGER NOT NULL,
+    FOREIGN KEY(id_cidade) REFERENCES cidade(id_cidade)
+    );
+
+SET @idCuritibaCidade := (SELECT id_cidade FROM cidade WHERE nome = 'Curitiba');
+SET @idSaoPauloCidade := (SELECT id_cidade FROM cidade WHERE nome = 'Sao Paulo');
+SET @idRiodeJaneiroCidade := (SELECT id_cidade FROM cidade WHERE nome = 'Rio de Janeiro');
+SET @idPetropolisCidade := (SELECT id_cidade FROM cidade WHERE nome = 'Petropolis');
+SET @idFeiradeSantanaCidade := (SELECT id_cidade FROM cidade WHERE nome = 'Feira de Santana');
+
+SELECT @idFeiradeSantanaCidade as 'cidade';
+
+INSERT INTO restaurantes (nome, descricao,id_cidade) VALUES
+    ('Comida Mineira', 'comida tipica de minas, noce nao viu o nome?',@idCuritibaCidade),
+    ('Fogo de chao', 'fogo... no chao?',@idSaoPauloCidade),
+    ('Frangao','Uma homenagem a frangos sei la nao to enxergando',@idRiodeJaneiroCidade),
+    ('Pizza Maromba','So pode comer aqui se voce usa bomba',@idPetropolisCidade),
+    ('Pertuti', 'Deve ser italiano',@idFeiradeSantanaCidade);
+
+SELECT 
+    * 
+FROM 
+    restaurantes 
+INNER JOIN
+    cidade ON cidade.id_cidade = restaurantes.id_cidade
+WHERE
+    restaurantes.nome = 'Pizza Maromba'
+;
+
+SELECT 
+    * 
+FROM 
+    cidade 
+INNER JOIN
+    restaurantes ON restaurantes.id_cidade=cidade.id_cidade 
+WHERE
+    restaurantes.nome = 'Pizza Maromba'
+;
+
+SELECT 
+    cidade.nome as nome_da_cidade,
+    restaurantes.nome as nome_do_restaurante,
+    restaurantes.descricao as descricao_do_restaurante
+FROM
+    cidade
+INNER JOIN
+    restaurantes
+        ON restaurantes.id_cidade = cidade.id_cidade
+WHERE
+    restaurantes.nome LIKE '%M%';
+
+SELECT 'transformando em uma tabela ---------' as 'INFO';
+
+SELECT
+    t1.nome_da_cidade,
+    t1.nome_do_restaurante
+FROM
+(
+        SELECT
+            cidade.nome as nome_da_cidade,
+            restaurantes.nome as nome_do_restaurante,
+            restaurantes.descricao as descricao_do_restaurante
+        FROM
+            cidade
+        INNER JOIN
+            restaurantes
+                ON restaurantes.id_cidade = cidade.id_cidade
+        WHERE
+            restaurantes.nome LIKE '%M%'
+) as t1
+
+WHERE
+    t1.nome_da_cidade = 'Curitiba'/*piora de performance, é usado para aplicar alguns switch cases, remoção de dados ou fazer joins com resultados de outros joins*/
+;
+
+SELECT 'com mais de um INNER JOIN --------' as 'INFO';
+
+SELECT 
+    cidade.nome as nome_da_cidade,
+    estado.nome as nome_do_estado,
+    restaurantes.nome as nome_do_restaurante
+FROM   
+    restaurantes
+INNER JOIN
+    cidade
+        ON restaurantes.id_cidade = cidade.id_cidade
+INNER JOIN
+    estado  
+        ON cidade.id_estado = estado.id_estado
+WHERE 
+    restaurantes.nome LIKE "Comida M%";
